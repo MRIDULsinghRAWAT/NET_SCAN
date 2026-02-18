d4de40d2c89e6604f523f43ebf59f37fdc2aafe8
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2026-02-18 22:46:38 +0530

    engine done

--- backend/app/scanner/engine.py
+++ backend/app/scanner/engine.py
@@ -3,22 +3,23 @@ import threading
 from queue import Queue
 
 # Configuration
-TARGET_IP = "192.168.1.1" 
-PORT_RANGE = range(1, 1025) 
-THREADS = 100 
+# defining the target IP and port range for scanning
+TARGET_IP = "192.168.1.1"  # machine by which we are scanning, can be changed to any IP
+PORT_RANGE = range(1, 1025)
+THREADS = 100  # can check 1000 ports with 100 threads
 
 queue = Queue()
 # Hum list ki jagah Dictionary use karenge taaki metadata (Version) store ho sake
 scan_results = {} 
 
-def get_banner(sock):
+def get_banner(sock):   #THIS IS THE UNIQUE FEATURE
     """
     Attempts to grab the service version banner from an open port.
     """
     try:
         # Kuch services ko trigger karne ke liye empty string bhejna padta hai
-        sock.send(b'Hello\r\n')
-        banner = sock.recv(1024).decode().strip()
+        sock.send(b'Hello\r\n') # sends a  knock to the service, sometimes required to get a response
+        banner = sock.recv(1024).decode(errors='ignore').strip() # BANNER GRABBING - service badle me apna naam aur version battai hai 
         return banner
     except:
         return "Unknown Service"
@@ -29,11 +30,11 @@ def port_scan(port):
         sock.settimeout(1.5) # Banner grab ke liye thoda zyada time
         result = sock.connect_ex((TARGET_IP, port))
         
-        if result == 0:
+        if result == 0:  # if result is 0, port is open
             version = get_banner(sock)
             print(f"[+] Port {port} is OPEN | Service: {version}")
             scan_results[port] = version
-        sock.close()
+        sock.close()  # otherwise the port is closed, we just ignore it
     except Exception:
         pass
 
